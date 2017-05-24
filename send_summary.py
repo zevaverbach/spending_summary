15d8ebc92224a32ff1c13e80d816ac6168c82940
zevav <zev@averba.ch>
2017-05-24 15:37:11 -0400

    make .md match .py

--- send_summary.py
+++ send_summary.py
@@ -8,7 +8,9 @@ twilio_client = TwilioClient(os.getenv('TWILIO_SID'), os.getenv('TWILIO_TOKEN'))
 
 def send_summary(transactions: List[dict]) -> None:
     total_spent = sum(transaction['amount'] for transaction in transactions)
-
     message = f'You spent ${total_spent} yesterday. 💸'
-
     twilio_client.api.account.messages.create(to=os.getenv('MY_CELL'), from_=os.getenv('MY_TWILIO_NUM'), body=message)
+
+
+if __name__ == "__main__":
+    send_summary(get_yesterdays())
